30daba296fbf5ccfa7f30bcbc95f4a1c3aa24263
Mihai Chitic <mihai.chitic@bunnyshell.com>
2023-06-12 16:01:56 +0300

    add Dockerfile and login script for the ttyd component

---
+++ .bunnyshell/argocd-ttyd/login.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+serverAddr=argocd-sever:443
+newPass=${ARGOCD_PASS}
+initialPass=${ARGOCD_INITIAL_PASS}
+
+if [[ ! -z "$initialPass" ]]; then
+  echo "loging with initial pass..."
+  argocd login --username admin --password $initialPass --insecure $serverAddr
+  if [ $? -eq 0]; then
+     echo "setting new pass..."
+     argocd account update-password --current-password $initialPass --new-password $newPass
+  else
+     echo "loging with new pass..."
+     argocd login --username admin --password $newPass --insecure $serverAddr
+  fi
+else
+  echo "loging with new pass..."
+  argocd login --username admin --password $newPass --insecure $serverAddr
+fi
+
+echo $?
