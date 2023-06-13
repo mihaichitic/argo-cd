44eb781f9a01e3ff39d06063843a58e5ce1c435f
Mihai Chitic <mihai.chitic@bunnyshell.com>
2023-06-13 18:29:25 +0300

    add also destroy scripts

--- .bunnyshell/argocd-ttyd/login.sh
+++ .bunnyshell/argocd-ttyd/login.sh
@@ -5,18 +5,17 @@ newPass=${ARGOCD_PASS}
 initialPass=${ARGOCD_INITIAL_PASS}
 
 if [[ ! -z "$initialPass" ]]; then
-  echo "loging with initial pass..."
+  echo "logging in with initial pass..."
   argocd login --username admin --password $initialPass --insecure $serverAddr
   if [ $? -eq 0 ]; then
      echo "setting new pass..."
      argocd account update-password --current-password $initialPass --new-password $newPass
   else
-     echo "loging with new pass..."
+     echo "logging in with new pass..."
      argocd login --username admin --password $newPass --insecure $serverAddr
   fi
 else
-  echo "loging with new pass..."
+  echo "logging in with new pass..."
   argocd login --username admin --password $newPass --insecure $serverAddr
 fi
 
-echo $?
