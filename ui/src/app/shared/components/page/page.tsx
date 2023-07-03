eecefbb4353082d06e896d00c0cf44f9d48c913a
mihaichitic <m.chitic+github@gmail.com>
2023-07-03 10:21:33 +0300

    Update page.tsx
    Signed-off-by: mihaichitic <m.chitic+github@gmail.com>

--- ui/src/app/shared/components/page/page.tsx
+++ ui/src/app/shared/components/page/page.tsx
@@ -26,7 +26,7 @@ export const AddAuthToToolbar = (init: Toolbar | Observable<Toolbar>, ctx: Conte
                     {loggedIn =>
                         loggedIn ? (
                             <button className='login-logout-button' key='logout' onClick={() => (window.location.href = requests.toAbsURL('/auth/logout'))}>
-                                Log out Mihai14
+                                Log out Mihai15
                             </button>
                         ) : (
                             <button className='login-logout-button' key='login' onClick={() => ctx.navigation.goto(`/login?return_url=${encodeURIComponent(location.href)}`)}>
